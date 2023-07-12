587b2a919b21e502fdd885c5390dea7cc3d9055e
yakovgavryushov <95687203+yakovgavryushov@users.noreply.github.com>
2023-07-12 17:54:38 +0300

    new genres are available

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const cheerio = require("cheerio");
 const app = express();
 
 // express-handlebars
-
 app.engine("handlebars", exphbr.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -18,30 +17,50 @@ app.use(express.static(path.join(__dirname, "public")));
 // list of what will be recieved
 const list = [];
 
+// genres
+const genres = [
+  {
+    name: "allbooks",
+    address: "https://www.labirint.ru/books/",
+  },
+  {
+    name: "english",
+    address: "https://www.labirint.ru/genres/2829/",
+  },
+  {
+    name: "fiction",
+    address: "https://www.labirint.ru/genres/1852/",
+  },
+  {
+    name: "non-fiction",
+    address: "https://www.labirint.ru/genres/3000/",
+  },
+];
+
+genres.forEach((genre) => {
+  axios.get(genre.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $("span.product-title", html).each(function () {
+      const title = $(this).text();
+      list.push({
+        title: title,
+        //   url: url,
+        name: "labyrinth",
+        genresource: genre.name,
+      });
+    });
+  });
+});
+// start page
 app.get("/", (req, res) => {
   res.render("start");
 });
 
-app.get("/allbooks", (req, res) => {
-  axios
-    .get("https://www.labirint.ru/books/")
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      $("span.product-title", html).each(function () {
-        const title = $(this).text();
-        // const url = $(this).attr("href");
-        list.push({
-          title: title,
-          //   url: url,
-          name: "labyrinth",
-        });
-      });
-
-      console.log(list);
-      res.render("allbooks", { list });
-    })
-    .catch((err) => console.log(err));
+// page with exact id
+app.get("/:id", (req, res) => {
+  res.render(`${req.params.id}`, { list });
 });
 
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
